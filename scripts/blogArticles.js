d17006d8dcb606710068a999db69098193cae495
Austin Rogers <contactALRogers@gmail.com>
2017-02-20 07:07:41 -0800

    updated

--- scripts/blogArticles.js
+++ scripts/blogArticles.js
@@ -6,5 +6,5 @@ var rawData = [
     subject:    'food',
     publishedOn: '2016-01-15',
     body:        '<p>The salmon cookies project was the first project I did where I struggled heavily as a student. Many of the concepts we went over I had major issues with and had to heavily rely on outside sources and my TAs and classmates.</p><p>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test </p><p>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test </p>'
-  },
+  },{"title":"adsgrbrv","body":"<p>waefsrdrsdas</p>\n","publishedOn":null,"daysAgo":17210,"publishStatus":"(draft)"},
 ];
